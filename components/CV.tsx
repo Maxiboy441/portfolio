0a0d32ffa8097bd99b801e238e9e11a119468005
Maximilian Huber <maximilian.huber@odt.net>
2025-06-30 10:38:49 +0200

    updated date

--- components/CV.tsx
+++ components/CV.tsx
@@ -135,7 +135,7 @@ export function CV() {
       ),
     },
     {
-      title: "18.08.2025",
+      title: "Sep 2025",
       content: (
         <div className="flex flex-col md:flex-row items-center mb-12">
           <div className="flex-1 pr-5">
